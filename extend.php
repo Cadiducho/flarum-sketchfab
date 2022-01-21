53d908afb8b5424b4e3460e7fb0440dccc9cea73
Cadiducho <cadiducho@gmail.com>
2022-01-21 15:02:33 +0100

    fix extend.php

--- extend.php
+++ extend.php
@@ -16,14 +16,16 @@ use s9e\TextFormatter\Configurator;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
+        ->js(__DIR__.'/js/dist/forum.js'),
+
     (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js')
+        ->js(__DIR__.'/js/dist/admin.js'),
+
     (new Extend\Formatter)
         ->configure(function (Configurator $config) {
             $config->BBCodes->addCustom(
                 '[sketchfab]{TEXT1}[/sketchfab]',
                 '<iframe allow="autoplay; fullscreen; xr-spatial-tracking" frameborder="0" width="640" height="480" mozallowfullscreen="true" webkitallowfullscreen="true" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/{TEXT1}/embed"></iframe>'
             );
-        })
+        }),
 ];
